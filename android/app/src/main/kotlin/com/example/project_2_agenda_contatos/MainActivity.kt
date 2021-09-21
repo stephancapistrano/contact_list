b7e950ddbffbfc3e2f91622b841630747ba75a10
stephancapistrano <stephancapistrano@gmail.com>
2021-09-20 23:28:51 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/project_2_agenda_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_2_agenda_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:project_2_agenda_contatos/ui/contact_page.dart';
+import 'package:project_2_agenda_contatos/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
